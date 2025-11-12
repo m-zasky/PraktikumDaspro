f65df884378e603c7210bde8da25a1432387283f
m-zasky <mmuhammadzasky@gmail.com>
2025-11-12 21:32:04 +0700

    Modifikasi

--- JOBSHEET-9/ArrayNilai18.java
+++ JOBSHEET-9/ArrayNilai18.java
@@ -5,13 +5,18 @@ public class ArrayNilai18 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke- " + i + " :");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            // System.out.println("Nilai akhir ke-" + i + "adalah " + nilaiAkhir[i]);
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " dinyatakan Lulus");
+            } else { //modifikasi untuk kondisi tidak lulus
+                System.out.println("Mahasiswa ke-" + i + " dinyatakan Tidak Lulus");
+            }
         }
 
         sc.close();
