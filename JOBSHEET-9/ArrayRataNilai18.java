7819437a441411ae697d53e0e83b589e4a427307
m-zasky <mmuhammadzasky@gmail.com>
2025-11-12 22:00:08 +0700

    ModifikasiNilaiLulus

--- JOBSHEET-9/ArrayRataNilai18.java
+++ JOBSHEET-9/ArrayRataNilai18.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class ArrayRataNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,18 +8,21 @@ public class ArrayRataNilai18 {
         double total = 0, rata2;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukan nilai mahasiswa ke- "+ (i + 1) + " : ");
+            System.out.print("Masukan nilai mahasiswa ke- " + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+
+            // modifikasi untuk menampilkan mahasiswa yang lulus
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i + 1) + " dinyatakan lulus");
+            }
         }
 
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai mahasiswa = " + rata2);
-        
-
 
         sc.close();
     }
