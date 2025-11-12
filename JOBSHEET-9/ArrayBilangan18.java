7c252f50f72fb6fae057959e91b21d7e591e8753
m-zasky <mmuhammadzasky@gmail.com>
2025-11-12 21:33:33 +0700

    ArrayBilangan18

--- JOBSHEET-9/ArrayBilangan18.java
+++ JOBSHEET-9/ArrayBilangan18.java
@@ -1,22 +1,15 @@
 public class ArrayBilangan18 {
     public static void main(String[] args) {
-        // int[] bil = new int [4];
-        // bil[0] = 5;
-        // bil[1] = 13;   
-        // bil[2] = -7;
-        // bil[3] = 17;
+        int[] bil = new int[4];
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
 
-        // modifikasi inisialisasi langsung pada array bil
-        int[] bil = {5, 13, -7, 17};
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
 
-        // System.out.println(bil[0]);
-        // System.out.println(bil[1]);
-        // System.out.println(bil[2]);
-        // System.out.println(bil[3]);
-
-        // modifikasi dengan perulangan for
-        for (int i = 0; i < 4; i++) {
-            System.out.println(bil[i]);
-        }
     }
 }
