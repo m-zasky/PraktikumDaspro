d484735e67f2b1f9eee5f9b7fe8cb6e07e0ec487
m-zasky <mmuhammadzasky@gmail.com>
2025-11-13 21:40:50 +0700

    Modif3

--- JOBSHEET-9/ArrayRataNilai18.java
+++ JOBSHEET-9/ArrayRataNilai18.java
@@ -3,34 +3,41 @@ import java.util.Scanner;
 public class ArrayRataNilai18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // agar user bisa menginputkan array jumlah mahasiswa
-        System.out.print("Masukkan jumlah mahasiswa : ");
-        int jumlahMahasiswa = sc.nextInt();
 
-        int[] nilaiMhs = new int[jumlahMahasiswa];
-        double sumLulus = 0, sumTidakLulus = 0;
-        int jumlahLulus = 0, jumlahTidakLulus = 0;
+        int[] nilaiMhs = new int[10];
+        double total = 0, rata2;
+        int max, min;
 
+        // Input nilai mahasiswa
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
 
+        // Inisialisasi nilai maksimum dan minimum
+        max = nilaiMhs[0];
+        min = nilaiMhs[0];
+
+        // Hitung total, nilai tertinggi, dan nilai terendah
         for (int i = 0; i < nilaiMhs.length; i++) {
-            if (nilaiMhs[i] > 70) {
-                sumLulus += nilaiMhs[i];
-                jumlahLulus++;
-            } else {
-                sumTidakLulus += nilaiMhs[i];
-                jumlahTidakLulus++;
+            total += nilaiMhs[i];
+
+            if (nilaiMhs[i] > max) {
+                max = nilaiMhs[i];
+            }
+
+            if (nilaiMhs[i] < min) {
+                min = nilaiMhs[i];
             }
         }
 
-        double rataLulus = (jumlahLulus > 0) ? (sumLulus / jumlahLulus) : 0;
-        double rataTidakLulus = (jumlahTidakLulus > 0) ? (sumTidakLulus / jumlahTidakLulus) : 0;
+        // Hitung rata-rata
+        rata2 = total / nilaiMhs.length;
 
-        System.out.printf("Rata-rata nilai lulus = %.1f\n", rataLulus);
-        System.out.printf("Rata-rata nilai tidak lulus = %.1f\n", rataTidakLulus);
+        // Tampilkan hasil
+        System.out.println("Rata-rata nilai mahasiswa = " + rata2);
+        System.out.println("Nilai tertinggi = " + max);
+        System.out.println("Nilai terendah = " + min);
 
         sc.close();
     }
