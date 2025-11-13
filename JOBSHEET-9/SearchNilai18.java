a93bd5e2a9dea958e465c039a8be11e394ccad23
m-zasky <mmuhammadzasky@gmail.com>
2025-11-13 22:12:43 +0700

    Modif1

--- JOBSHEET-9/SearchNilai18.java
+++ JOBSHEET-9/SearchNilai18.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class SearchNilai18 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlah = sc.nextInt();
+
+        int[] arrNilai = new int[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("\nMasukkan nilai yang akan dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1;
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
+            if (arrNilai[i] == key) {
                 hasil = i;
                 break;
             }
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        if (hasil != -1) {
+            System.out.println("\nNilai " + key + " ketemu, merupakan nilai mahasiswa ke- " + (hasil + 1));
+        }
+        sc.close();
     }
 }
