61c3efa0b57a7b78dfaeb240e0548c77c9a75578
m-zasky <mmuhammadzasky@gmail.com>
2025-11-19 22:18:06 +0700

    Numbers18

---
+++ JOBSHEET-10/Numbers18.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Numbers18 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+        // Penambahan kode
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        // Penambahan kode2
+        for (int i = 0; i < myNumbers.length; i++) {
+        System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
+    }
+}
