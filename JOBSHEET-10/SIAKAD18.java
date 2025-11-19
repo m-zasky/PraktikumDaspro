53a7259e8335587e5d5e1ce6b24348192908b25c
m-zasky <mmuhammadzasky@gmail.com>
2025-11-19 22:56:51 +0700

    SIAKAD18

---
+++ JOBSHEET-10/SIAKAD18.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SIAKAD18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // INPUT JUMLAH SISWA DAN MATA KULIAH
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+        // BUAT ARRAY DINAMIS
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+        // INPUT NILAI PER SISWA
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("\nInput nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Rata-rata nilai mahasiswa ke-" + (i + 1) + ": "
+                    + totalPerSiswa / jumlahMatkul);
+        }
+
+        // HITUNG RATA-RATA PER MATA KULIAH
+        System.out.println("\n===============================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
+
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPerMatkul = 0;
+
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPerMatkul += nilai[i][j];
+            }
+
+            System.out.println("Mata Kuliah " + (j + 1) + ": " + (int) totalPerMatkul / jumlahSiswa);
+        }
+
+        sc.close();
+    }
+}
